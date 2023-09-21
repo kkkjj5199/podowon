20d3073cab874c6ad566021647c5ad1544bf6b20
yujin <kkkjj5199@naver.com>
2023-09-22 00:23:49 +0900

    0922

--- js/digital.js
+++ js/digital.js
@@ -107,9 +107,18 @@
 
     //플레이어 변수 설정
     var player;
+    var player2;
     var myaudio = document.getElementById('myaudio');
     function onYouTubeIframeAPIReady() {
         player = new YT.Player('player', {
+            videoId: 'zlLQMyuz9YE',
+            events: {
+                'onReady': onPlayerReady,//로딩중에 이벤트 실행한다
+                'onStateChange': onPlayerStateChange//플레이어 상태 변화 시 이벤트를 실행한다.
+
+            }
+        });
+        player2 = new YT.Player('player2', {
             videoId: 'PWxGSNSQhOE',
             events: {
                 'onReady': onPlayerReady,//로딩중에 이벤트 실행한다
