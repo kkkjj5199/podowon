696be09e3146720881da52f8a02b8152e708b4d7
kkkjj5199 <55727481+kkkjj5199@users.noreply.github.com>
2023-09-27 13:58:01 +0900

    Update chuseok.js

--- js/chuseok.js
+++ js/chuseok.js
@@ -54,49 +54,10 @@
 
      
       
-        const btnPrev = document.querySelector('.carousel-control-prev');
-        const btnNext = document.querySelector('.carousel-control-next');
+    
        
-        let slides = document.querySelectorAll('.carousel-item');
-        let currentIdx = 0;
-
        
-        
-        function c1(){
-            goToSlide(currentIdx -1 );
-        }
 
-         function c2(){
-            goToSlide(currentIdx + 1);
-        }
-
-        function goToSlide(currentIdx) {
-            updateNav(currentIdx);
-        }
-
-        goToSlide(0);
-
-        function updateNav(currentIdx) {
-            btnPrev.classList.add('disabled');
-            // if(slides[0] || currentIdx == 0){
-            //     btnPrev.classList.add('disabled');
-            // }else{
-            //     btnPrev.classList.remove('disabled');
-            // }
-            // if(currentIdx ==0 ){
-            //     btnPrev.classList.add('disabled');
-            //     console.log(currentIdx);
-            // }else {
-            //     btnPrev.classList.remove('disabled');
-            // }
-
-            // if(currentIdx == slideCount-1 ){
-            //     btnNext.classList.add('disabled');
-            // }else {
-            //     btnNext.classList.remove('disabled');
-            //     console.log(currentIdx);
-            // }
-        }
     //노래 시작 시간
 
 
@@ -108,7 +69,7 @@
 
     //플레이어 변수 설정
     var player;
-    var player2;
+    
     var myaudio = document.getElementById('myaudio');
     function onYouTubeIframeAPIReady() {
         player = new YT.Player('player', {
@@ -119,14 +80,7 @@
 
             }
         });
-        player2 = new YT.Player('player2', {
-            videoId: '1UJZnimXK-U',
-            events: {
-                'onReady': onPlayerReady,//로딩중에 이벤트 실행한다
-                'onStateChange': onPlayerStateChange//플레이어 상태 변화 시 이벤트를 실행한다.
-
-            }
-        });
+    
     }
 
     function onPlayerReady(event) {
