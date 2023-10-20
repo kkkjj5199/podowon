118ac911a8c71959fafa15eb2cc9022c01e1674f
yujin <kkkjj5199@naver.com>
2023-10-20 09:10:52 +0900

    1020 슬라이드

--- js/digital.js
+++ js/digital.js
@@ -137,7 +137,7 @@
         for (var i = 1; i < 29; i++) {
             var img_src = 'asset/image/digital/감사/감사-' + i + '.png';
 
-                $("#images").append('<div class="carousel-item" > <img src=' + img_src + '></div>    ');
+                $("#images").append('<div class="carousel-item" > <img id="i3" class="d-block w-100"  src=' + img_src + '></div>    ');
 
         }
 
