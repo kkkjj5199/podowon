570b7edbffae31450d4b863be3eb5d724c3d9365
yujin <kkkjj5199@naver.com>
2023-10-28 12:09:29 +0900

    새가족슬라이드

--- js/digital.js
+++ js/digital.js
@@ -135,9 +135,9 @@
     }
 
     // <div>태그 반복문
-        // var dd = document.createElement("div");
-        // for (var i = 1; i < 42; i++) {
-        //     var img_src = 'asset/image/digital/감사/감사-' + i + '.png';
-        //       $("#images").append('<div class="carousel-item" > <img id="i3" class="d-block w-100"  src=' + img_src + '></div>');
-        // }
+        var dd = document.createElement("div");
+        for (var i = 0; i <=6; i++) {
+            var img_src = 'asset/image/digital_pre/포도원한가족-' + i + '.jpg';
+              $("#images").append('<div class="carousel-item" > <img id="i3" class="d-block w-100"  src=' + img_src + '></div>');
+        }
 
