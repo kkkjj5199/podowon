078126bc328fd1447e29340c7483ec891516c130
yujin <kkkjj5199@naver.com>
2023-12-02 23:18:36 +0900

    묵상집

--- js/all.js
+++ js/all.js
@@ -1,67 +1,11 @@
     let sound = document.querySelector('audio');
     sound.currentTime= 18;
-/* Google map*/
-        // var container = document.getElementById('map');
-        // var options = {
-        //     center: new kakao.maps.LatLng(37.367729, 126.965929),
-        //     level: 2
-        // };
-        // var map = new kakao.maps.Map(container, options);
-        // // 마커가 표시될 위치입니다 
-        // var markerPosition = new kakao.maps.LatLng(37.367729, 126.965929);
-
-        // // 마커를 생성합니다
-        // var marker = new kakao.maps.Marker({
-        //     position: markerPosition,
-        // });
-
-        // marker.setDraggable(true);
-
-
-        // // 마커가 지도 위에 표시되도록 설정합니다
-        // marker.setMap(map);
-
-
-        // var content = '<div class="customoverlay">' +
-        //     '  <a href="http://kko.to/pNYElrMIGU" target="_blank">' +
-        //     '    <span class="title">포도원 교회</span>' +
-        //     '  </a>' +
-        //     '</div>';
-
-        // var position = markerPosition
-        // var customOverlay = new kakao.maps.CustomOverlay({
-        //     map: map,
-        //     position: position,
-        //     content: content,
-        //     yAnchor: 1
-        // });
-
-
-        // kakao.maps.event.addListener(marker, 'click', function () {
-        //     // 마커 위에 인포윈도우를 표시합니다
-        //     window.open('http://kko.to/pNYElrMIGU');
-        // });
 
 
         const btnPrev = document.querySelector('.carousel-control-prev');
         const btnNext = document.querySelector('.carousel-control-next');
 
        
-        // let slides = document.querySelectorAll('.carousel-item');
-        // let currentIdx = 0;
-
-
-        // function goToSlide() {
-        //     updateNav();
-        // }
-
-        // goToSlide(0);
-
-        // function updateNav() {
-        //     btnPrev.classList.add('disabled');
-          
-        // }
-   
 
 
     //youtube API 불러오는 부분
@@ -152,6 +96,13 @@
             var img_src = 'asset/image/all/묵상집/' + i + '.webp';
               $("#M2").append('<div class="carousel-item" > <img id="i3" class="d-block w-100"  src=' + img_src + '></div>');
         }
+
+           for (var i = 0; i <=25; i++) {
+            var img_src = 'asset/image/all/묵상집/' + i + '.webp';
+              $("#MM").append('<img class="swiper-slide"  src=' + img_src + '></img>');
+        }
+
+
 
 
 
