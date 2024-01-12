437c2f29ef6d35cfa32a92af3b9dec92f8751476
yujin <kkkjj5199@naver.com>
2024-01-12 13:01:44 +0900

    기관별 추가

--- js/digital.js
+++ js/digital.js
@@ -140,10 +140,7 @@
 
     // <div>태그 반복문
        
-        for (var i = 1; i <=2; i++) {
-            var img_src = 'asset/image/digital/새가족-' + i + '.webp';
-              $("#newMember").append('<div class="carousel-item" > <img id="i3" class="d-block w-100"  src=' + img_src + '></div>');
-        }
+
 
        
         for (var i = 1; i <=4; i++) {
@@ -151,50 +148,60 @@
               $("#pres").append('<div class="carousel-item" > <img id="i3" class="d-block w-100"  src=' + img_src + '></div>');
         }
 
-        for (var i = 1; i <=12; i++) {
-            var img_src = 'asset/image/digital/아동3_' + i + '.webp';
-              $("#kids3Images").append('<div class="carousel-item" > <img id="i3" class="d-block w-100"  src=' + img_src + '></div>');
+
+        // 아동1
+         for (var i = 1; i <=8; i++) {
+            var img_src = 'asset/image/digital_pre/adong1/' + i + '.webp';
+              $("#1part").append('<div class="carousel-item" > <img id="i3" class="d-block w-100"  src=' + img_src + '></div>');
         }
 
+
+        // 아동3
+       for (var i = 2; i <=19; i++) {
+            var img_src = 'asset/image/digital_pre/adong3/' + i + '.webp';
+              $("#3part").append('<div class="carousel-item" > <img id="i3" class="d-block w-100"  src=' + img_src + '></div>');
+        }
+
+    //  중등부
+       for (var i = 2; i <=8; i++) {
+            var img_src = 'asset/image/digital_pre/middlepart/' + i + '.webp';
+              $("#mpart").append('<div class="carousel-item" > <img id="i3" class="d-block w-100"  src=' + img_src + '></div>');
+        }
+
+     //  고등부
+       for (var i = 2; i <=13; i++) {
+            var img_src = 'asset/image/digital_pre/highpart/' + i + '.webp';
+              $("#hpart").append('<div class="carousel-item" > <img id="i3" class="d-block w-100"  src=' + img_src + '></div>');
+        }
+
+    
+        //  청년회
+       for (var i = 2; i <=13; i++) {
+            var img_src = 'asset/image/digital_pre/caryouth/' + i + '.webp';
+              $("#youthpart").append('<div class="carousel-item" > <img id="i3" class="d-block w-100"  src=' + img_src + '></div>');
+        }
+
+
                 
-        // for (var i = 1; i <=4; i++) {
-        //     var img_src = 'asset/image/digital/기_청년회의밤_' + i + '.webp';
-        //       $("#caryouth").append('<div class="carousel-item" > <img id="i3" class="d-block w-100"  src=' + img_src + '></div>');
-        // }
+  
 
     
  // Sample list of image URLs
         const advertises = [
             'asset/image/digital/ad__p.webp',
-            'asset/image/digital/신년주일.webp',
-            'asset/image/digital/당역구역회.webp',
-            'asset/image/digital/주일저녁예배.webp',
             'asset/image/digital/2024헌신예배.webp',
             'asset/image/digital/임원세미나.webp',
-             'asset/image/digital/교역자회의.webp',
+           
               'asset/image/digital/기부금.webp',
             
-            // Add more image URLs as needed
+            
         ];
 
         const story = [
         'asset/image/digital/포도원스토리.webp',
-        'asset/image/digital/송구영신예배_1.webp',
-        'asset/image/digital/송구영신예배_2.webp',
-        'asset/image/digital/송구영신예배_3.webp',
-        'asset/image/digital/송구영신예배_4.webp',
-        'asset/image/digital/송구영신예배_5.webp',
-        'asset/image/digital/송구영신예배_6.webp',
-        'asset/image/digital/송구영신리멤버.webp', 
-        'asset/image/digital/신년축복성회_1.webp',
-        'asset/image/digital/신년축복성회_2.webp',
-        'asset/image/digital/신년축복성회_3.webp',
-        'asset/image/digital/신년축복성회_4.webp',
-        'asset/image/digital/신년축복성회_5.webp',
-        'asset/image/digital/신년축복성회_6.webp',
-        'asset/image/digital/신년축복성회_7.webp',
 
-            // Add more image URLs as needed
+
+          
         ];
 
           $('[id^="imageCarouselModal"]').on('hidden.bs.modal', function () {
