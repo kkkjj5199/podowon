92f284b4ac4ef12f8e090e863997cdf8f94e726a
yujin <kkkjj5199@naver.com>
2024-02-24 15:08:57 +0900

    정식

--- js/digital.js
+++ js/digital.js
@@ -167,21 +167,21 @@
 
         // 아동1
          for (var i = 2; i <=17; i++) {
-            var img_src = 'asset/image/digital_pre/adong1/' + i + '.webp';
+            var img_src = 'asset/image/digital/adong1/' + i + '.webp';
               $("#1part").append('<div class="carousel-item" > <img id="i3" class="d-block w-100"  src=' + img_src + '></div>');
         }
 
         
         // 아동2
          for (var i = 2; i <=12; i++) {
-            var img_src = 'asset/image/digital_pre/adong2/' + i + '.webp';
+            var img_src = 'asset/image/digital/adong2/' + i + '.webp';
               $("#2part").append('<div class="carousel-item" > <img id="i3" class="d-block w-100"  src=' + img_src + '></div>');
         }
 
 
         // 아동3
        for (var i = 2; i <=18; i++) {
-            var img_src = 'asset/image/digital_pre/adong3/' + i + '.webp';
+            var img_src = 'asset/image/digital/adong3/' + i + '.webp';
               $("#3part").append('<div class="carousel-item" > <img id="i3" class="d-block w-100"  src=' + img_src + '></div>');
         }
 
@@ -221,7 +221,7 @@
             //     itemContent = '<iframe id="' + iframe_id + '" class="d-block w-100" src="' + youtube_link + '" frameborder="0" allowfullscreen style="width:100%; height:315px;"></iframe>';
             // } else {
                 // Insert image
-                var img_src = 'asset/image/digital_pre/middlepart/' + i + '.webp';
+                var img_src = 'asset/image/digital/middlepart/' + i + '.webp';
                 itemContent = '<img class="d-block w-100" src="' + img_src + '">';
             // }
 
@@ -245,7 +245,7 @@
             //     itemContent = '<iframe id="' + iframe_id + '" class="d-block w-100" src="' + youtube_link + '" frameborder="0" allowfullscreen style="width:100%; height:315px;"></iframe>';
             // } else {
                 // Insert image
-                var img_src = 'asset/image/digital_pre/highpart/' + i + '.webp';
+                var img_src = 'asset/image/digital/highpart/' + i + '.webp';
                 itemContent = '<img class="d-block w-100" src="' + img_src + '">';
             // }
 
@@ -267,20 +267,20 @@
 
     
         //  청년회
-       for (var i = 2; i <=14; i++) {
-            var img_src = 'asset/image/digital_pre/caryouth/' + i + '.webp';
+       for (var i = 2; i <=18; i++) {
+            var img_src = 'asset/image/digital/caryouth/' + i + '.webp';
               $("#youthpart").append('<div class="carousel-item" > <img id="i3" class="d-block w-100"  src=' + img_src + '></div>');
         }
 
         // 여선교회
              for (var i = 2; i <=8; i++) {
-            var img_src = 'asset/image/digital_pre/woman/' + i + '.webp';
+            var img_src = 'asset/image/digital/woman/' + i + '.webp';
               $("#womanpart").append('<div class="carousel-item" > <img id="i3" class="d-block w-100"  src=' + img_src + '></div>');
         }
 
         // 남선교회
              for (var i = 2; i <=5; i++) {
-            var img_src = 'asset/image/digital_pre/man/' + i + '.webp';
+            var img_src = 'asset/image/digital/man/' + i + '.webp';
               $("#manpart").append('<div class="carousel-item" > <img id="i3" class="d-block w-100"  src=' + img_src + '></div>');
         }
 
@@ -293,12 +293,12 @@
         const advertises = [
         'asset/image/common/ad__p.webp',
       
-         'asset/image/digital_pre/사순절.webp',
-        'asset/image/digital_pre/졸업예배.webp',
-        'asset/image/digital_pre/서울소년원.webp',
-        'asset/image/digital_pre/새달새.webp',
-        'asset/image/digital_pre/새학기부흥성회.webp',
-        'asset/image/digital_pre/여선교회축복성회.webp',
+         'asset/image/digital/사순절.webp',
+        'asset/image/digital/졸업예배.webp',
+        'asset/image/digital/서울소년원.webp',
+        'asset/image/digital/새달새.webp',
+        'asset/image/digital/새학기부흥성회.webp',
+        'asset/image/digital/여선교회축복성회.webp',
    
      
        
@@ -307,21 +307,21 @@
 
         const story = [
         'asset/image/common/포도원스토리.webp',
-        'asset/image/digital_pre/옥타묘띳-1.webp',
-        'asset/image/digital_pre/옥타묘띳-2.webp',
-        'asset/image/digital_pre/옥타묘띳-3.webp',
-        'asset/image/digital_pre/옥타묘띳-4.webp',
-        'asset/image/digital_pre/옥타묘띳-5.webp',
-        'asset/image/digital_pre/옥타묘띳-6.webp',
-        'asset/image/digital_pre/미1.webp',
-        'asset/image/digital_pre/미2.webp',
-        'asset/image/digital_pre/미3.webp',
-        'asset/image/digital_pre/미4.webp',
-        'asset/image/digital_pre/미5.webp',
-        'asset/image/digital_pre/미6.webp',
-        'asset/image/digital_pre/미7.webp',
-        'asset/image/digital_pre/미8.webp',
-        'asset/image/digital_pre/미9.webp'
+        'asset/image/digital/옥타묘띳-1.webp',
+        'asset/image/digital/옥타묘띳-2.webp',
+        'asset/image/digital/옥타묘띳-3.webp',
+        'asset/image/digital/옥타묘띳-4.webp',
+        'asset/image/digital/옥타묘띳-5.webp',
+        'asset/image/digital/옥타묘띳-6.webp',
+        'asset/image/digital/미1.webp',
+        'asset/image/digital/미2.webp',
+        'asset/image/digital/미3.webp',
+        'asset/image/digital/미4.webp',
+        'asset/image/digital/미5.webp',
+        'asset/image/digital/미6.webp',
+        'asset/image/digital/미7.webp',
+        'asset/image/digital/미8.webp',
+        'asset/image/digital/미9.webp'
         
   
 
