f99550f34651df999901c749d158f836155d6eec
yujin <kkkjj5199@naver.com>
2024-03-02 10:03:47 +0900

    pre

--- js/digital.js
+++ js/digital.js
@@ -132,8 +132,8 @@
     // <div>태그 반복문
 
         //말씀
-         frame2(21,'adong1','#1part');
-        for (var i = 1; i <=5; i++) {
+       
+        for (var i = 1; i <=4; i++) {
             var img_src = 'asset/image/common/pre_' + i + '.webp';
               $("#pres").append('<div class="carousel-item" > <img id="i3" class="d-block w-100"  src=' + img_src + '></div>');
         }
