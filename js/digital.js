ab0f0c9629614b90f1066e8cc2e6f3e241ef114f
SHIN YOO <98073889+YooShin-with-Joy@users.noreply.github.com>
2024-04-26 20:18:30 +0900

    아동3

--- js/digital.js
+++ js/digital.js
@@ -165,7 +165,7 @@
 
 
         // 아동3
-        frame2(40,'adong3','#3part');
+        frame2(34,'adong3','#3part');
         // for (var i = 2; i <= 36; i++) {
         //     var carouselInner = $("#3part");  
         //     var itemContent;
