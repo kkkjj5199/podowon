7868281c0ab758b8894ea9f9b5b2711ee0a921f1
김유진 <yujin@gim-yujin-ui-noteubug.local>
2024-05-10 18:43:52 +0900

    common?

--- js/digital.js
+++ js/digital.js
@@ -277,6 +277,12 @@
         'asset/image/digital/2.webp',
         'asset/image/digital/3.webp',
         'asset/image/digital/4.webp',
+         'asset/image/digital/5.webp',
+          'asset/image/digital/6.webp',
+           'asset/image/digital/7.webp',
+            'asset/image/digital/8.webp',
+             'asset/image/digital/9.webp',
+
       
         
         ]
