a2af4bd9c71dfad7ea2bf8985edca7c55b35f2cb
sj-woooon <167854640+sj-woooon@users.noreply.github.com>
2024-06-26 16:55:10 +0900

    Update digital.js

--- js/digital.js
+++ js/digital.js
@@ -231,7 +231,7 @@
         // }
 
         //  청년회
-        frame2(5,'caryouth','#youthpart');
+        frame2(15,'caryouth','#youthpart');
 
         // 여선교회
         frame2(6,'woman','#womanpart');
