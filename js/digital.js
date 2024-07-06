e44c44c5b3cd96e0b4dcd8d285465e4143659768
podowon <168050841+podowon@users.noreply.github.com>
2024-07-06 12:09:14 +0900

    Update digital.js
    남선교회 개수 업로드 수정

--- js/digital.js
+++ js/digital.js
@@ -238,7 +238,7 @@
 
 
         // 남선교회
-        frame2(4,'man','#manpart');
+        frame2(3,'man','#manpart');
         // for (var i = 2; i <=4; i++) {
         // var h = $("#manpart"); 
         //    var itemContent;
