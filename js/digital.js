98c2d04b555d086d1d3a486af6a339c4192f2e7b
kkkjj5199 <kkkjj5199@naver.com>
2024-07-27 00:30:26 +0900

    map 분리

--- js/digital.js
+++ js/digital.js
@@ -1,51 +1,6 @@
     // let sound = document.querySelector('audio');
     // sound.currentTime= 1;
 
-
-
-/* Google map 지도 */ 
-        var container = document.getElementById('map');
-        var options = {
-            center: new kakao.maps.LatLng(37.367729, 126.965929),
-            level: 2
-        };
-        var map = new kakao.maps.Map(container, options);
-        // 마커가 표시될 위치입니다 
-        var markerPosition = new kakao.maps.LatLng(37.367729, 126.965929);
-
-        // 마커를 생성합니다
-        var marker = new kakao.maps.Marker({
-            position: markerPosition,
-        });
-
-        marker.setDraggable(true);
-
-
-        // 마커가 지도 위에 표시되도록 설정합니다
-        marker.setMap(map);
-
-
-        var content = '<div class="customoverlay">' +
-            '  <a href="http://kko.to/pNYElrMIGU" target="_blank">' +
-            '    <span class="title">포도원 교회</span>' +
-            '  </a>' +
-            '</div>';
-
-        var position = markerPosition
-        var customOverlay = new kakao.maps.CustomOverlay({
-            map: map,
-            position: position,
-            content: content,
-            yAnchor: 1
-        });
-
-
-        kakao.maps.event.addListener(marker, 'click', function () {
-            // 마커 위에 인포윈도우를 표시합니다
-            window.open('http://kko.to/pNYElrMIGU');
-        });
-
-
         const btnPrev = document.querySelector('.carousel-control-prev');
         const btnNext = document.querySelector('.carousel-control-next');
 
@@ -144,7 +99,7 @@
          
     }
 
-    // <div>태그 반복문
+
 
         //말씀
         for (var i = 1; i <=4; i++) {
