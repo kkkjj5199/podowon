c69b7b1d1257a232560aec2c91ecac886496beb1
dmsquf601 <168065462+dmsquf601@users.noreply.github.com>
2024-08-01 11:25:39 +0900

    Update digital.js
    여선교회 5로 수정

--- js/digital.js
+++ js/digital.js
@@ -138,7 +138,7 @@
         frame2(10,'caryouth','#youthpart');
 
         //여선교회
-        frame2(8,'woman','#womanpart');
+        frame2(5,'woman','#womanpart');
 
         //남선교회
         frame2(5,'man','#manpart');
