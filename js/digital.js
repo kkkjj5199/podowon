c4fa88dbb0c46ea2517422e7f51b2ea46efaec6b
kkkjj5199 <kkkjj5199@naver.com>
2024-08-02 21:42:18 +0900

    common&high

--- js/digital.js
+++ js/digital.js
@@ -132,7 +132,7 @@
          frame2(18,'middlepart','#mpart');
 
         //고등부 교구
-        frame2(11,'highpart','#hpart');
+        frame2(13,'highpart','#hpart');
 
         //청년회 교구
         frame2(14,'caryouth','#youthpart');
