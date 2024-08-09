3ec1db861601c1cff3549a85af3c51783847e026
kkkjj5199 <kkkjj5199@naver.com>
2024-08-09 14:36:27 +0900

    common

--- js/digital.js
+++ js/digital.js
@@ -144,7 +144,7 @@
         frame2(5,'man','#manpart');
 
         //포도원마을소식
-        frame2(7,'notice','#notice');
+        frame2(6,'notice','#notice');
 
         //포도원스토리
         // frame2(28,'pdwstory','#pwdstory');
