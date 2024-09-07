111ffafc00a1293d28cdf02b1cb8ba52a6460c47
dlwjdals5901 <167318308+dlwjdals5901@users.noreply.github.com>
2024-09-07 09:10:49 +0900

    Update digital.js
    고등부 11개로 수정

--- js/digital.js
+++ js/digital.js
@@ -132,7 +132,7 @@
         frame2(13,'middlepart','#mpart');
         
         //고등부 교구
-        frame2(13,'highpart','#hpart');
+        frame2(11,'highpart','#hpart');
 
         //청년회 교구
         frame2(4,'caryouth','#youthpart');
