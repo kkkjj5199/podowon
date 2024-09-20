8c8cbb84ed1ae4181bd825e057b02ae7915f74eb
kkkjj5199 <kkkjj5199@naver.com>
2024-09-21 01:58:40 +0900

    v2

--- js/digital.js
+++ js/digital.js
@@ -144,10 +144,10 @@
         frame2(4,'man','#manpart');
 
         //포도원마을소식
-        frame2(7,'notice','#notice');
+        frame2(5,'notice','#notice');
 
         //포도원스토리
-        frame2(34,'pdwstory','#pwdstory');
+        frame2(10,'pdwstory','#pwdstory');
         // youtubeFrame(9,4,'pdwstory','#pwdstory');
 
 
