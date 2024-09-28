07f4bdad5bfa1d769c5e69e4144a2c2d916f9fb8
YSH-1123 <ysh41659545@gmail.com>
2024-09-28 12:52:51 +0900

    중등부 20으로 수정

--- js/digital.js
+++ js/digital.js
@@ -129,7 +129,7 @@
         frame3(214,'adong3','#3part');
 
         //중등부 교구
-        frame2(23,'middlepart','#mpart');
+        frame2(20,'middlepart','#mpart');
         
         //고등부 교구
         frame2(12,'highpart','#hpart');
