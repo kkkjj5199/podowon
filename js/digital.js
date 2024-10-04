e7bc40bc9909d757767cc045b5cfed3e00728fd4
SHIN YOO <98073889+YooShin-with-Joy@users.noreply.github.com>
2024-10-04 19:14:11 +0900

    아동3

--- js/digital.js
+++ js/digital.js
@@ -123,7 +123,7 @@
         //youtubeFrame(29,7,'https://youtu.be/-ndskXoVepE?si=MW6-Ou4B2OUMkbQL','adong2','#2part');
 
         // 아동3교구
-        frame2(21,'adong3','#3part');
+        frame2(16,'adong3','#3part');
         // frame4(5,'adong3','#3part');
 
         frame3(214,'adong3','#3part');
@@ -214,7 +214,7 @@
 
                  // 아동3 슬라이드-preVer.
         function frame3(length,partName,idName){
-            for(var i =205; i<= length; i++){
+            for(var i =195; i<= length; i++){
               var img_src = 'asset/image/digital/' + partName+ '/' + i + '.webp';
                $(idName).append('<div class="carousel-item" > <img id="i3" class="d-block w-100"  src=' + img_src + '></div>');
             }
