5e1f61d53571e37c19f53f85dfe3ee3d0185c693
podowon <168050841+podowon@users.noreply.github.com>
2024-10-26 14:09:03 +0900

    Update digital.js

--- js/digital.js
+++ js/digital.js
@@ -141,7 +141,7 @@
         frame2(7,'woman','#womanpart');
 
         //남선교회
-        frame2(5,'man','#manpart');
+        frame2(4,'man','#manpart');
 
         //포도원마을소식
         frame2(5,'notice','#notice');
