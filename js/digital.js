e5742e70fc05ac55a62513eecbd77f7385ef572e
minhg5816 <167869082+minhg5816@users.noreply.github.com>
2024-11-01 17:02:38 +0900

    아동2 22로 수정
    아동2 22로 수정

--- js/digital.js
+++ js/digital.js
@@ -119,7 +119,7 @@
         frame2(30,'adong1','#1part');
 
         // 아동2교구
-        frame2(19,'adong2','#2part');
+        frame2(22,'adong2','#2part');
         //youtubeFrame(29,7,'https://youtu.be/-ndskXoVepE?si=MW6-Ou4B2OUMkbQL','adong2','#2part');
 
         // 아동3교구
