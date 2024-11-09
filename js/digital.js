bd18ecc60d51ff3ca623754ebe033dee741030f2
kkkjj5199 <kkkjj5199@naver.com>
2024-11-09 12:03:58 +0900

    v1

--- js/digital.js
+++ js/digital.js
@@ -102,7 +102,7 @@
 
 
         //말씀
-        for (var i = 1; i <=5; i++) {
+        for (var i = 1; i <=4; i++) {
             var img_src = 'asset/image/common/pre_' + i + '.webp';
               $("#pres").append('<div class="carousel-item" > <img id="i3" class="d-block w-100"  src=' + img_src + '></div>');
         }
@@ -144,7 +144,7 @@
         frame2(4,'man','#manpart');
 
         //포도원마을소식
-        frame2(8,'notice','#notice');
+        frame2(10,'notice','#notice');
         
 
       
@@ -155,7 +155,7 @@
         // youtubeFrameList(23,numberList,linkList,'pdwstory','#pdwstory'); 
 
      //포도원스토리
-        frame2(8,'pdwstory','#pdwstory');
+        frame2(7,'pdwstory','#pdwstory');
   
 
 
