5c56c13a2a0723661664c67a2d464bd27ac34cad
YSH-1123 <ysh41659545@gmail.com>
2024-12-06 20:58:59 +0900

    중20

--- js/digital.js
+++ js/digital.js
@@ -129,7 +129,7 @@
         frame3(203,'adong3','#3part');
 
         //중등부 교구
-        frame2(19,'middlepart','#mpart');
+        frame2(20,'middlepart','#mpart');
         
         //고등부 교구
         frame2(10,'highpart','#hpart');
