178422787a2273eabfc5cb347dd496d89809bc7c
kkkjj5199 <kkkjj5199@naver.com>
2024-12-21 23:46:19 +0900

    s

--- js/digital.js
+++ js/digital.js
@@ -102,7 +102,7 @@
 
 
         //말씀
-        for (var i = 1; i <=4; i++) {
+        for (var i = 1; i <=5; i++) {
             var img_src = 'asset/image/common/pre_' + i + '.webp';
               $("#pres").append('<div class="carousel-item" > <img id="i3" class="d-block w-100"  src=' + img_src + '></div>');
         }
