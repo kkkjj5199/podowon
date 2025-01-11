acfccbb7fc6b7fe4c480ade800a9083f16b919c5
kkkjj5199 <kkkjj5199@naver.com>
2025-01-11 13:08:26 +0900

    중등부 영상 추가

--- js/digital.js
+++ js/digital.js
@@ -129,7 +129,8 @@
         frame3(209,'adong3','#3part');
 
         //중등부 교구
-        frame2(15,'middlepart','#mpart');
+        // frame2(15,'middlepart','#mpart');
+        youtubeFrame(15,9,'https://youtu.be/gtbN63lQioA?si=mm0IvcowjzQXDb2Y','middlepart','#mpart');
         
         //고등부 교구
         frame2(26,'highpart','#hpart');
