b2405f3eded14568ac6e297ad26727b7918cfb67
dlwjdals5901 <167318308+dlwjdals5901@users.noreply.github.com>
2025-01-11 10:58:20 +0900

    Update digital.js
    고등부 수정

--- js/digital.js
+++ js/digital.js
@@ -132,7 +132,7 @@
         frame2(7,'middlepart','#mpart');
         
         //고등부 교구
-        // frame2(26,'highpart','#hpart');
+        frame2(26,'highpart','#hpart');
          
 
         //청년회 교구
