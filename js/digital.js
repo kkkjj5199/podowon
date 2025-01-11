0b00fda2550130911e256b9c23ec412436d5999e
kkkjj5199 <kkkjj5199@naver.com>
2025-01-11 14:51:24 +0900

    청년회

--- js/digital.js
+++ js/digital.js
@@ -159,7 +159,7 @@
         // youtubeFrameList(23,numberList,linkList,'pdwstory','#pdwstory'); 
 
      //포도원스토리
-        frame2(11,'pdwstory','#pdwstory');
+        frame2(3,'pdwstory','#pdwstory');
   
 
 
