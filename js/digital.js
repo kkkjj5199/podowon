eacb30acd2d698970392cda2768ebcf8cdc34c1b
minhg5816 <167869082+minhg5816@users.noreply.github.com>
2025-01-25 08:56:27 +0900

    아동2 23 수정

--- js/digital.js
+++ js/digital.js
@@ -119,7 +119,7 @@
         frame2(14,'adong1','#1part');
 
         // 아동2교구
-        frame2(21,'adong2','#2part');
+        frame2(23,'adong2','#2part');
         //youtubeFrame(29,7,'https://youtu.be/-ndskXoVepE?si=MW6-Ou4B2OUMkbQL','adong2','#2part');
 
         // 아동3교구
