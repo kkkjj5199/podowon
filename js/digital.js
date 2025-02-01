3a3b6399444aec31c4d6cb59f17ac1de1485feb2
YSH-1123 <ysh41659545@gmail.com>
2025-02-01 11:47:05 +0900

    Update digital.js

--- js/digital.js
+++ js/digital.js
@@ -129,7 +129,7 @@
         frame3(209,'adong3','#3part');
 
         //중등부 교구
-        frame2(27,'middlepart','#mpart');
+        frame2(14,'middlepart','#mpart');
         // youtubeFrame(15,9,'https://youtu.be/gtbN63lQioA?si=mm0IvcowjzQXDb2Y','middlepart','#mpart');
         
         //고등부 교구
