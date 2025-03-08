ef0d71aba4729fb8d3b277081ae79b03c8f936db
dlwjdals5901 <167318308+dlwjdals5901@users.noreply.github.com>
2025-03-08 15:25:56 +0900

    Update digital.js
    아동 3 21로 수정

--- js/digital.js
+++ js/digital.js
@@ -124,7 +124,7 @@
         //youtubeFrame(29,7,'https://youtu.be/-ndskXoVepE?si=MW6-Ou4B2OUMkbQL','adong2','#2part');
 
         // 아동3교구
-        frame2(16,'adong3','#3part');
+        frame2(21,'adong3','#3part');
         // frame4(13,'adong3','#3part');
 
         frame5(118,'adong3','#3part');
