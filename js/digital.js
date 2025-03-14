af4d75cb077db9a2f4c72ce1f58e5d2ebf78bd83
podowon <168050841+podowon@users.noreply.github.com>
2025-03-14 18:13:39 +0900

    Update digital.js
    남선교회 소식 업로드

--- js/digital.js
+++ js/digital.js
@@ -146,7 +146,7 @@
         frame2(9,'woman','#womanpart');
 
         //남선교회
-        frame2(5,'man','#manpart');
+        frame2(3,'man','#manpart');
 
       
         // gotoYoutube("CGBSXJGD5P8vQyr0")
