e573c427dc939c25773cae0d20b361309d6e5e1d
kkkjj5199 <kkkjj5199@naver.com>
2025-03-22 16:18:13 +0900

    v3

--- js/digital.js
+++ js/digital.js
@@ -165,7 +165,7 @@
         // youtubeFrameList(23,numberList,linkList,'pdwstory','#pdwstory'); 
 
      //포도원스토리
-        frame2(4,'pdwstory','#pdwstory');
+        frame2(7,'pdwstory','#pdwstory');
           
         
   
