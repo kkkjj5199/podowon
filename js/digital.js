a5a8aa5752fc516aa3cc8918f40e9d15bd4922ba
podowon <168050841+podowon@users.noreply.github.com>
2025-04-12 15:59:24 +0900

    Update digital.js

--- js/digital.js
+++ js/digital.js
@@ -146,7 +146,7 @@
         frame2(6,'woman','#womanpart');
 
         //남선교회
-        frame2(3,'man','#manpart');
+        frame2(6,'man','#manpart');
 
       
         // gotoYoutube("CGBSXJGD5P8vQyr0")
