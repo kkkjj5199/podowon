a1d8e67265620aebb505b1ef71b92193a0455ccb
SHIN YOO <98073889+YooShin-with-Joy@users.noreply.github.com>
2025-05-17 11:19:17 +0900

    Update digital.js

--- js/digital.js
+++ js/digital.js
@@ -124,7 +124,7 @@
         //youtubeFrame(29,7,'https://youtu.be/-ndskXoVepE?si=MW6-Ou4B2OUMkbQL','adong2','#2part');
 
         // 아동3교구
-        frame2(10,'adong3','#3part');
+        frame2(17,'adong3','#3part');
         // frame4(17,'adong3','#3part');
 
         frame5(119,'adong3','#3part');
