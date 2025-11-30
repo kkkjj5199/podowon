22ca068a48c47ea88cc1532a6136d18f7bfa7dd2
kkkjj5199 <kkkjj5199@naver.com>
2025-11-30 16:02:44 +0900

    청밤추가

--- js/map.js
+++ js/map.js
@@ -2,13 +2,13 @@
 
     var container = document.getElementById('map');
         var options = {
-            center: new kakao.maps.LatLng(37.367729, 126.965929),
+            center: new kakao.maps.LatLng(37.39284, 126.95304),
             level: 2
         };
         var map = new kakao.maps.Map(container, options);
         // 마커가 표시될 위치입니다 
-        var markerPosition = new kakao.maps.LatLng(37.367729, 126.965929);
-
+        var markerPosition = new kakao.maps.LatLng(37.39284, 126.95304); //더 트리니티
+        // var markerPosition = new kakao.maps.LatLng(37.367729, 126.965929); 포도원교회
         // 마커를 생성합니다
         var marker = new kakao.maps.Marker({
             position: markerPosition,
@@ -21,9 +21,10 @@
         marker.setMap(map);
 
 
+
         var content = '<div class="customoverlay">' +
-            '  <a href="http://kko.to/pNYElrMIGU" target="_blank">' +
-            '    <span class="title">포도원 교회</span>' +
+            '  <a href="https://place.map.kakao.com/13143585" target="_blank">' +
+            '    <span class="title">안양 더 트리니티</span>' +
             '  </a>' +
             '</div>';
 
@@ -38,5 +39,5 @@
 
         kakao.maps.event.addListener(marker, 'click', function () {
             // 마커 위에 인포윈도우를 표시합니다
-            window.open('http://kko.to/pNYElrMIGU');
+            window.open('https://place.map.kakao.com/13143585');
         });
